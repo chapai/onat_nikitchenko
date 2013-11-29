dc5d3aeada4cd74e8822fb0fbd6599315613ddcf
chapai <chepizhenko.alex@gmail.com>
2013-11-29 09:46:08 +0200

    Comments added

--- Part1/client_udp.c
+++ Part1/client_udp.c
@@ -8,7 +8,9 @@
 
 void calculateUsingServer(char *address, char *port, float startFrom, float calculateTo, float delta)
 {
+    // Structure for server address
     struct sockaddr_in servaddr;
+    
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = inet_addr(address);
@@ -16,28 +18,37 @@ void calculateUsingServer(char *address, char *port, float startFrom, float calc
     
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     
+    // Argument buffer
     float currentArgument = startFrom;
+
+    // Result buffer
     float result;
 
+    // While argument in the proper range
     while(currentArgument <= calculateTo)
     {
+        // Sending argument to the server
         sendto(sockfd, &currentArgument, sizeof(float), 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+        // Recieving result from the server 
         recvfrom(sockfd, &result, sizeof(float), 0, NULL, NULL);
         printf("Received result: %f for argument %f \n", result, currentArgument);
         
-        // Next argument
+        // Increasing argument by delta for the next step
         currentArgument += delta;
     } 
 }
 
 int main(int argc, char **argv)
 {
+    // If no console arguments provided
     if (argc != 3)
     {
         printf("Usage: client_udp <IP address> <port>\n");
         exit(1);
     }
 
+    // Input data
     float startFrom, calculateTo, delta;
     
     printf("Lower bound: ");
--- Part1/server_udp.c
+++ Part1/server_udp.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//just for false false and true
+//just for false and true
 #include <stdbool.h>
 
 #include <sys/socket.h>
@@ -13,23 +13,39 @@
 void startServer(char *address, char *port)
 {
     int sockfd, n;
+    // Structures for client and server address
     struct sockaddr_in servaddr, cliaddr;
     socklen_t len;
+    // Buffer for recieved arguments
     float recievedArgument;
     
+    // Creating UDB socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     
+    // Itiating server address structure by zzeros
     bzero(&servaddr, sizeof(servaddr));
+    
+    // Socket type
     servaddr.sin_family = AF_INET;
+
+    // Socket address (IPv4)
     servaddr.sin_addr.s_addr = inet_addr(address);
+
+    // Socket port
     servaddr.sin_port = htons(atoi(port));
+
+    // Binding socket on address
     bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
     
+    // Starting endless cycle
     while (true)
     {
         len = sizeof(cliaddr);  
         
+        // Waiting for data
         n = recvfrom(sockfd, &recievedArgument, sizeof(float), 0, (struct sockaddr *)&cliaddr, &len);
+
+        // If recieve error error
         if (n <= 0)
         {
             printf("42?\n");        
@@ -38,16 +54,19 @@ void startServer(char *address, char *port)
         
         printf("Received argument: %f \n", recievedArgument);
         
+        // Calculating result
         float result = 2.0f * ((1.0f - cosf(2.0f * recievedArgument)) / 2.0f) - ((1.0f + cosf(2.0f * recievedArgument)) / 2.0f);
         
         printf("Sending back: %f \n", result);
         
+        // Sending result to the client
         sendto(sockfd, &result, sizeof(float), 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
     }
 }
 
 int main(int argc, char **argv)
 {
+    // If no console arguments provided
     if (argc != 3)
     {
         printf("Usage: server_udp <IP address> <port>\n");
